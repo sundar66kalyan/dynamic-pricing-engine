249ea248d558ec852f25b93807472cdd370fa0aa
sundar66kalyan <132656131+sundar66kalyan@users.noreply.github.com>
2026-03-13 17:06:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import joblib
-import pandas as pd
+import numpy as np
 
 # Load model
 model = joblib.load("dynamic_pricing_model.pkl")
@@ -12,19 +12,13 @@ inventory = st.slider("Inventory Level",0,500)
 demand = st.slider("Demand Forecast",0,500)
 competitor = st.slider("Competitor Pricing",0,200)
 
-# Create dataframe
-input_data = pd.DataFrame({
-    "Inventory Level":[inventory],
-    "Demand Forecast":[demand],
-    "Competitor Pricing":[competitor]
-})
-
 # Prediction
 if st.button("Predict Price"):
+
+    input_data = np.array([[inventory, demand, competitor]])
+
     price = model.predict(input_data)
 
     st.subheader("Recommended Price")
-
     st.metric("Optimal Price", round(price[0],2))
-
     st.success(f"Recommended Price: {price[0]:.2f}")
